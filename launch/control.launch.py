c51bb0956d7218e434336ee766678e00f71d1814
aky <akiyugata271@gmail.com>
2025-06-19 19:23:16 +0900

    状態の変更もできた

--- launch/control.launch.py
+++ launch/control.launch.py
@@ -1,5 +1,8 @@
 import launch
 from launch_ros.actions import LifecycleNode
+from launch_ros.events.lifecycle import ChangeState
+import lifecycle_msgs
+import lifecycle_msgs.msg
 
 def generate_launch_description():
     ld = launch.LaunchDescription()
@@ -11,6 +14,15 @@ def generate_launch_description():
         namespace='yasarobo'
     )
 
+    to_configure = launch.actions.EmitEvent(
+        event = ChangeState(
+            lifecycle_node_matcher=launch.events.matches_action(joy_vel_convert),
+            transition_id=lifecycle_msgs.msg.Transition.TRANSITION_CONFIGURE,
+        )
+    )
+
+
     ld.add_action(joy_vel_convert)
+    ld.add_action(to_configure)
 
     return ld
